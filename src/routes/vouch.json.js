df0016d68e8cebe729d9ea1f3f4a533bf9c25d23
Tom Wilson <tom@hyper.io>
2022-07-29 15:36:59 +0000

    added vouch json

---
+++ src/routes/vouch.json.js
@@ -0,0 +1,19 @@
+import vouch from '$lib/vouch'
+
+export async function POST(event) {
+  try {
+    console.log('Got Signed Request!')
+    const body = await event.request.json()
+    const result = await vouch(body)
+    console.log('success!')
+    return {
+      body: result
+    }
+  } catch (e) {
+    console.log(e.message)
+    return {
+      status: 500,
+      body: { message: 'Error trying to process vouch request!' }
+    }
+  }
+}
